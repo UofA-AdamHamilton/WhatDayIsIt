e1f5d7fd7f373e6b163e3b5d6144d77e4114363a
Adam <adamhhamilton@gmail.com>
2025-04-15 20:06:32 +0930

    new files

---
+++ Questions.py
@@ -0,0 +1,15 @@
+import sys
+from Doomsday import *
+
+
+def get_sum_of_num(num1, num2, num3):
+    return (int(num1) + int(num2) + int(num3))
+
+
+if __name__ == "__main__":
+    # input
+
+    input1 = input('input number please ')
+
+    # output
+    print(input1)
